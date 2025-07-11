55e550da10b47b58798dbbf12178dcb996f83e64
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-07-11 01:56:38 -0300

    Corrigindo um bug
    Ocorreu um erro na codificação dos caracteres dos inserts

--- Treino de SQL/Values Clidb.sql
+++ Treino de SQL/Values Clidb.sql
@@ -1,13 +1,14 @@
 INSERT INTO dbo.clidb
 ([Codigo],[Nome],[Endereco],[UF],[Cidade])
 VALUES
-( 1, N'Miguel', N'Rua das Rosas', N'MG', N'Joaquim Felicio'),
-( 2, N'Rafael', N'Rua das Margaridas', N'SP', N'Uberlandia'),
-( 3, N'Gabriel', N'Rua das Orquideas', N'BA', N'Salvador'),
-( 4, N'André', N'Rua dos Cravos', N'PB', N'Jo�o Pessoa'),
-( 5, N'Jo�o', N'Rua das Azal�ias', N'RR', N'Dinossaurop�lis'),
+( 1, N'Miguel', N'Rua das Rosas', N'MG', N'Joaquim Felício'),
+( 2, N'Rafael', N'Rua das Margaridas', N'SP', N'Uberlândia'),
+( 3, N'Gabriel', N'Rua das Orquídeas', N'BA', N'Salvador'),
+( 4, N'André', N'Rua dos Cravos', N'PB', N'João Pessoa'),
+( 5, N'João', N'Rua das Azaleias', N'RR', N'Dinossauropólis'),
 ( 6, N'Santos', N'Rua das Jasmines', N'RG', N'Gramado'),
 ( 7, N'Nicolas', N'Rua das Tulipas', N'MT', N'Fernando Pessoa'),
-( 8, N'Lucas', N'Rua das Papoulas', N'SC', N'Espirito Santo'),
-( 9, N'Mariana', N'Rua dos Girass�is', N'RJ', N'Botafogo'),
-( 10, N'Karine', N'Rua dos Jardineiros', N'CE', N'Guadalupe')
+( 8, N'Lukas', N'Rua das Papoulas', N'SC', N'Espírito Santo'),
+( 9, N'Mariana', N'Rua dos Lírios Brancos', N'RJ', N'Botafogo'),
+(10, N'Karinne', N'Rua dos Jardineiros', N'CE', N'Guadalupe')
+GO
